273caf2b2ab64f98cb76cc2d4e256da172ef7c2f
Mike Heft <mikeheft@gmail.com>
2020-05-05 08:52:36 -0600

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Mike Heft on 05/05/2020.
+//  Copyright © 2020 Mike Heft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
